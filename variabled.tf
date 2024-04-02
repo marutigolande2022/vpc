bca0127d51fabad54236e9eca90fa27a9ea7b24f
marutigolande2022 <163817171+marutigolande2022@users.noreply.github.com>
2024-04-02 16:04:57 +0530

    Create variabled.tf

---
+++ variabled.tf
@@ -0,0 +1,24 @@
+variable "sg_name" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "ingress1_cidr_ipv4" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "ingress2_cidr_ipv4" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "ingress1_cidr_ipv6" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
+
+variable "egress1_cidr_blocks" {
+   type        = string
+   description = "Ubuntu AMI ID in N. Virginia Region"
+}
